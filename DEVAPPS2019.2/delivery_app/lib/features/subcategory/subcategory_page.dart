7b141e58edc858c7d1808d52f9c207e142f1ee6b
Rafael Lucena <rafael.lucena@arctouch.com>
2020-11-12 20:38:34 -0300

    Finish SubCategoryScreen

--- DEVAPPS2019.2/delivery_app/lib/features/subcategory/subcategory_page.dart
+++ DEVAPPS2019.2/delivery_app/lib/features/subcategory/subcategory_page.dart
@@ -9,10 +9,16 @@ class SubCategoryModel {
   double price;
   String currency;
   String unit;
+  bool isFavorite;
   String imageName;
 
   SubCategoryModel(
-      {this.title, this.price, this.currency, this.unit, this.imageName});
+      {this.title,
+      this.price,
+      this.currency,
+      this.unit,
+      this.isFavorite,
+      this.imageName});
 }
 
 class SubCategoryPage extends StatelessWidget {
@@ -23,18 +29,161 @@ class SubCategoryPage extends StatelessWidget {
         price: 1.10,
         currency: "€",
         unit: "piece",
+        isFavorite: false,
         imageName: "images/subcategory_placeholder.png"),
     SubCategoryModel(
         title: "Purple Cauliflower",
         price: 1.85,
         currency: "€",
         unit: "kg",
+        isFavorite: true,
+        imageName: "images/subcategory_placeholder.png"),
+    SubCategoryModel(
+        title: "Boston Lettuce",
+        price: 1.10,
+        currency: "€",
+        unit: "piece",
+        isFavorite: false,
+        imageName: "images/subcategory_placeholder.png"),
+    SubCategoryModel(
+        title: "Purple Cauliflower",
+        price: 1.85,
+        currency: "€",
+        unit: "kg",
+        isFavorite: true,
+        imageName: "images/subcategory_placeholder.png"),
+    SubCategoryModel(
+        title: "Boston Lettuce",
+        price: 1.10,
+        currency: "€",
+        unit: "piece",
+        isFavorite: false,
+        imageName: "images/subcategory_placeholder.png"),
+    SubCategoryModel(
+        title: "Purple Cauliflower",
+        price: 1.85,
+        currency: "€",
+        unit: "kg",
+        isFavorite: true,
+        imageName: "images/subcategory_placeholder.png"),
+    SubCategoryModel(
+        title: "Boston Lettuce",
+        price: 1.10,
+        currency: "€",
+        unit: "piece",
+        isFavorite: false,
+        imageName: "images/subcategory_placeholder.png"),
+    SubCategoryModel(
+        title: "Purple Cauliflower",
+        price: 1.85,
+        currency: "€",
+        unit: "kg",
+        isFavorite: true,
+        imageName: "images/subcategory_placeholder.png"),
+    SubCategoryModel(
+        title: "Boston Lettuce",
+        price: 1.10,
+        currency: "€",
+        unit: "piece",
+        isFavorite: false,
+        imageName: "images/subcategory_placeholder.png"),
+    SubCategoryModel(
+        title: "Purple Cauliflower",
+        price: 1.85,
+        currency: "€",
+        unit: "kg",
+        isFavorite: true,
+        imageName: "images/subcategory_placeholder.png"),
+    SubCategoryModel(
+        title: "Boston Lettuce",
+        price: 1.10,
+        currency: "€",
+        unit: "piece",
+        isFavorite: false,
+        imageName: "images/subcategory_placeholder.png"),
+    SubCategoryModel(
+        title: "Purple Cauliflower",
+        price: 1.85,
+        currency: "€",
+        unit: "kg",
+        isFavorite: true,
+        imageName: "images/subcategory_placeholder.png"),
+    SubCategoryModel(
+        title: "Boston Lettuce",
+        price: 1.10,
+        currency: "€",
+        unit: "piece",
+        isFavorite: false,
+        imageName: "images/subcategory_placeholder.png"),
+    SubCategoryModel(
+        title: "Purple Cauliflower",
+        price: 1.85,
+        currency: "€",
+        unit: "kg",
+        isFavorite: true,
+        imageName: "images/subcategory_placeholder.png"),
+    SubCategoryModel(
+        title: "Boston Lettuce",
+        price: 1.10,
+        currency: "€",
+        unit: "piece",
+        isFavorite: false,
+        imageName: "images/subcategory_placeholder.png"),
+    SubCategoryModel(
+        title: "Purple Cauliflower",
+        price: 1.85,
+        currency: "€",
+        unit: "kg",
+        isFavorite: true,
+        imageName: "images/subcategory_placeholder.png"),
+    SubCategoryModel(
+        title: "Boston Lettuce",
+        price: 1.10,
+        currency: "€",
+        unit: "piece",
+        isFavorite: false,
+        imageName: "images/subcategory_placeholder.png"),
+    SubCategoryModel(
+        title: "Purple Cauliflower",
+        price: 1.85,
+        currency: "€",
+        unit: "kg",
+        isFavorite: true,
+        imageName: "images/subcategory_placeholder.png"),
+    SubCategoryModel(
+        title: "Boston Lettuce",
+        price: 1.10,
+        currency: "€",
+        unit: "piece",
+        isFavorite: false,
+        imageName: "images/subcategory_placeholder.png"),
+    SubCategoryModel(
+        title: "Purple Cauliflower",
+        price: 1.85,
+        currency: "€",
+        unit: "kg",
+        isFavorite: true,
+        imageName: "images/subcategory_placeholder.png"),
+    SubCategoryModel(
+        title: "Boston Lettuce",
+        price: 1.10,
+        currency: "€",
+        unit: "piece",
+        isFavorite: false,
+        imageName: "images/subcategory_placeholder.png"),
+    SubCategoryModel(
+        title: "Purple Cauliflower",
+        price: 1.85,
+        currency: "€",
+        unit: "kg",
+        isFavorite: true,
         imageName: "images/subcategory_placeholder.png"),
     SubCategoryModel(
         title: "Savoy Cabbage",
         price: 1.45,
         currency: "€",
         unit: "g",
+        isFavorite: true,
         imageName: "images/subcategory_placeholder.png"),
   ];
 
@@ -73,6 +222,8 @@ class SubCategoryPage extends StatelessWidget {
   }
 }
 
+enum SubCategoryTextSize { small, medium, large }
+
 class SubCategoryCell extends StatelessWidget {
   final SubCategoryModel model;
 
@@ -80,19 +231,83 @@ class SubCategoryCell extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          model.title,
-        ),
-        Row(
-          children: [
-            Text("${model.price}"),
-            Text("${model.currency} / ${model.unit}"),
-          ],
-        )
-      ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      child: Row(
+        children: [
+          Container(
+              height: 110,
+              width: 110 * 1.3828125,
+              child: Image.asset("images/subcategory_placeholder.png")),
+          SizedBox(width: 20),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              _createText(data: model.title, size: SubCategoryTextSize.medium),
+              SizedBox(
+                height: 12,
+              ),
+              Row(
+                children: [
+                  _createText(
+                      data: "${model.price}", size: SubCategoryTextSize.large),
+                  SizedBox(
+                    width: 2,
+                  ),
+                  _createText(
+                      data: "${model.currency} / ${model.unit}",
+                      size: SubCategoryTextSize.small),
+                ],
+              ),
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Container(
+                    height: 40,
+                    width: 60,
+                    child: Image.asset(
+                        "images/${model.isFavorite ? "is_favorite" : "non_favorite"}.png"),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Container(
+                    height: 40,
+                    width: 60,
+                    child: Image.asset("images/cart.png"),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ],
+      ),
     );
+  }
+
+  _createText({String data, SubCategoryTextSize size}) {
+    TextStyle style;
+
+    switch (size) {
+      case SubCategoryTextSize.small:
+        style = TextStyle(fontSize: 16, color: AppColors.secondaryTextColor);
+        break;
+      case SubCategoryTextSize.medium:
+        style = TextStyle(
+            fontSize: 18,
+            color: AppColors.primaryTextColor,
+            fontWeight: FontWeight.w500);
+        break;
+      case SubCategoryTextSize.large:
+        style = TextStyle(
+            fontSize: 22,
+            color: AppColors.primaryTextColor,
+            fontWeight: FontWeight.bold);
+        break;
+    }
+
+    return Text(data, style: style);
   }
 }
